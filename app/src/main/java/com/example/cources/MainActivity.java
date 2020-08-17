d54c0a9aa7aef0bbed306302e27c57c3a26e6a14
David Sulava <sulavaperm@gmail.com>
2020-08-17 15:15:41 +0500

    added Model for the anime_db

--- app/src/main/java/com/example/cources/MainActivity.java
+++ app/src/main/java/com/example/cources/MainActivity.java
@@ -7,11 +7,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import io.realm.Realm;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Realm.init(this);
         setContentView(R.layout.activity_main);
 
     }
